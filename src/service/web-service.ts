842ae164f2669ec5fccc04f04bb3373aa6fec296
Daman Sharma <damansharma8k@gmail.com>
2025-01-09 19:41:16 +0530

    Adds web service methods

---
+++ src/service/web-service.ts
@@ -0,0 +1,17 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
+
+export const GET = <T = any>(url: string, config?: AxiosRequestConfig) => {
+    return axios.get<T>(url, config);
+}
+
+export const POST = <ReqT = any, ResT = any>(url: string, body: ReqT, config?: AxiosRequestConfig) => {
+    return axios.post<ResT>(url, body, config);
+}
+export const PUT = <ReqT = any, ResT = any>(url: string, body: ReqT, config?: AxiosRequestConfig) => {
+    return axios.put<ResT>(url, body, config);
+}
+export const DELETE = <ResT = any>(url: string, config?: AxiosRequestConfig) => {
+    return axios.delete<ResT>(url,config);
+}
