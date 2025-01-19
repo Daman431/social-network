2a34561c449c44c90684eae1637c47cd3d63bac3
Daman Sharma <damansharma8k@gmail.com>
2025-01-19 15:39:56 +0530

    Updates textfield icons

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { HeaderProps } from "./HeaderProps";
+
+const Header: FC<HeaderProps> = ({ className }) => {
+    return <div className="bg-primary-100 text-white h-1/6 p-4">
+        Header
+    </div>
+}
+
+export default Header;
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import { LayoutProps } from "./LayoutProps";
 
 const Layout: FC<LayoutProps> = ({ children, className }) => {
-    return <div className={`h-screen w-screen bg-slate-100 ${className}`}>
+    return <div className={`h-screen w-screen grid grid-cols-1 bg-slate-100 ${className}`}>
         {/* Header Start*/}
         {/* Header End */}
         {children}
